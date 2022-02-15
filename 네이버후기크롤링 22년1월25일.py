e1b6aaa1be80732cd0d7b94b93cc6741f37e46b0
developer-hyun <rlckdrkdud@naver.com>
2022-02-15 14:11:18 +0900

    0215-2

--- 네이버후기크롤링 22년1월25일.py
+++ 네이버후기크롤링 22년1월25일.py
@@ -12,7 +12,15 @@ def crilling():
 
 
     file_name = "1월25일테스트본"      #입력받은 파일이름을 가져오는 함수
-    url = "https://smartstore.naver.com/deproject/products/672496361?"             #입력받은 URL을 저장하는 함수.
+  #  url = "https://smartstore.naver.com/deproject/products/672496361?"       #디프로젝트 디불      #입력받은 URL을 저장하는 함수.
+    #url = "https://smartstore.naver.com/siestern/products/5319139287?"         #스턴미키
+    #url = "https://shopping.naver.com/play/play/stores/100566881/products/4736711991"  #에이비방향제 #사이트가다름
+    #url = "https://smartstore.naver.com/store-ab/products/5214229210?"  #에이비방향제 도베르만 메탈타입
+   # url = "https://smartstore.naver.com/l-kshop/products/5205442576?" #쓰리나인방향제
+    #url = "https://smartstore.naver.com/scentmonster/products/5108811877?" #센트몬스터방향제
+    url = "https://smartstore.naver.com/gti/products/4748622246"  #밴볼릭 방향제
+
+
     Se = Service('C:/Users/rlckd/PycharmProjects/colling/chromedriver.exe')
     binary = "chromedriver.exe"       #크롬창을 키기위해 이것 필요.
 
@@ -24,66 +32,21 @@ def crilling():
     excel_sheet = excel_file.active
     excel_sheet.append(['score', 'message', 'date', 'option_one', 'option_two', 'option_three'])  # 매장마다 바뀜
     excel_sheet.column_dimensions['B'].width = 100
+    time.sleep(1)
+    driver.find_element(By.XPATH,"/html/body/div/div/div[3]/div[2]/div[2]/div/div[3]/div[3]/ul/li[2]/a").click()
+    time.sleep(1)
+
+
     #평점 낮은순 클릭
     #print(driver.find_element_by_xpath("/html/body/div/div/div[3]/div[2]/div[2]/div/div[3]/div[6]/div/div[3]/div/div[1]/div[1]/ul/li[4]/a").text)
-    driver.find_element(By.XPATH,"//*[@id='REVIEW']/div/div[3]/div/div[1]/div[1]/ul/li[4]").click()
+    #driver.find_element(By.XPATH,"/html/body/div/div/div[3]/div[2]/div[2]/div/div[3]/div[6]/div/div[3]/div/div[1]/div[1]/ul/li[4]/a").click()
     #
+
+    #평점높은순 클릭
+    driver.find_element(By.XPATH,"//*[@id='REVIEW']/div/div[3]/div/div[1]/div[1]/ul/li[3]").click()
     driver.implicitly_wait(10)
 
 
-
-    for page in range(3,35):
-        time.sleep(1)
-        review_data = driver.find_elements(By.XPATH,
-            '//*[@id="REVIEW"]/div/div[3]/div/div[2]/ul/li/div/div[1]/div/div[1]/div/div[1]/div[2]/div/span')  # 리뷰 텍스트 내용 선택
-        driver.implicitly_wait(20)
-        revier_star = driver.find_elements(By.XPATH,
-            '//*[@id="REVIEW"]/div/div[3]/div/div[2]/ul/li/div/div[1]/div/div[1]/div/div[1]/div[1]/div[2]/div[1]/em')  # 평점이 몇점인지 선택ㄱ
-        review_date = driver.find_elements(By.XPATH,
-            '//*[@id="REVIEW"]/div/div[3]/div/div[2]/ul/li/div/div[1]/div/div[1]/div/div[1]/div[1]/div[2]/div[2]/span')
-        review_op_fu_total = driver.find_elements(By.XPATH,
-            '//*[@id="REVIEW"]/div/div[3]/div/div[2]/ul/li/div/div[1]/div/div[1]/div/div[1]/div[1]/div[2]/div[3]/div/button/span')
-        driver.implicitly_wait(20)
-
-        for (j, k, l, s) in zip(revier_star, review_data, review_date,
-                                review_op_fu_total):  # 리뷰 텍스트 내용이랑 평점 몇점인지 내용 을 for문으로 하나하나 씩 가져온다.
-            l = l.text.replace('.', '')
-
-            driver.implicitly_wait(20)
-            excel_sheet.append([j.text, k.text + "\n", l, s.text])  # 엑셀에다가 내용을 추가함.
-            driver.implicitly_wait(20)
-
-        if page<=12:
-            pass
-        if 13<=page<=22:
-            page-=10
-        if 23<=page<=32:
-            page-=20
-        if 33<=page<=42:
-            page-=30
-        print(page)
-       # print(page)
-       # print("/html/body/div/div/div[3]/div[2]/div[2]/div/div[3]/div[6]/div/div[3]/div/div[2]/div/div/a[%s]"%str(page))
-        driver.find_elements(By.XPATH,"/html/body/div/div/div[3]/div[2]/div[2]/div/div[3]/div[6]/div/div[3]/div/div[2]/div/div/a[%s]"%str(page))[0].click()
-
-        #driver.find_element(By.CSS_SELECTOR,"#REVIEW > div > div._2y6yIawL6t > div > div.cv6id6JEkg > div > div > a:nth-child(%s)"%str(page)).click()
-        print("페이지")
-        time.sleep(1)
-
-
-    #
-    #
-    excel_file.save("{}.xlsx".format("테스트버전"))
-    excel_file.close()
-    time.sleep(1)
-    time.sleep(1000)
-
-
-
-
-
-    #Total_review_data = driver.find_elements_by_css_selector('#area_review_list > div.header_review._review_list_header > strong > span')
-    driver.implicitly_wait(10)
 
 
 if __name__ == '__main__':
